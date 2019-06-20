7cfd7580d0fbb0f16510ab1ab0f43a232575a207
Nurishka <nurishkav1998@gmail.com>
2019-06-20 03:28:04 +0300

    Initial Commit

---
+++ locationView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  locationView
+//
+//  Created by admin on 20/06/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
