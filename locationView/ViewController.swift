ec3e590f073258480fa06c5a9c81a18268f79751
Nurishka <nurishkav1998@gmail.com>
2019-07-02 19:43:00 +0300

    commit

--- locationView/ViewController.swift
+++ locationView/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mpView: MKMapView!
+    
+     let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+    
+    func setupLocationManader() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+    }
+    
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mpView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManader()
+            checkLocationAutorization()
+        } else{
+//           fgbgh
+        }
+    }
+    
+    func checkLocationAutorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mpView.showsUserLocation = true
+            let regionInMeters: Double = 10000
+            locationManager.stopUpdatingLocation()
+            break
+        case .denied:
+//          Show an alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+//            fgb
+            break
+        case .authorizedAlways:
+            break
+        }
     }
 
 
+}
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: CLLocation) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: Location.coordinate.latitude, longitude: locations.coordinate.latitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mpView.setRegion(region, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAutorization()
+    }
 }
 
